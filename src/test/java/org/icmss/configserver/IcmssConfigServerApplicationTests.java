19aa6e8b7e9893333985a52386eb41776ad7d488
ShaqMG <shalva.mgebrishvili.1@gmail.com>
2024-04-06 14:29:43 +0400

    Initial commit

---
+++ src/test/java/org/icmss/configserver/IcmssConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.icmss.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcmssConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
